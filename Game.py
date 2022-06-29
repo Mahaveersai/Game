5eaa42b340a50c32d1900ac83c17e071726a5288
Mahaveersai <108386177+Mahaveersai@users.noreply.github.com>
2022-06-29 01:39:48 -0400

    added comments

--- Game.py
+++ Game.py
@@ -1,8 +1,8 @@
 # Game
 #Player needs to pass few rounds given and by the end the player should have atleast min health of 1.The player can use the Armour given
 
-power = [1,2,3,6]
-armor = 7 
+power = [1,2,3,6] #given power levels
+armor = 7 #given armor
 total = sum(power)
 armourUsed = 0
 
@@ -13,5 +13,5 @@ for i in power:
   elif armourUsed < i:
     armourUsed = i
     
-print(total - armourUsed + 1)
+print(total - armourUsed + 1) # +1 is because to maintain min health
   
